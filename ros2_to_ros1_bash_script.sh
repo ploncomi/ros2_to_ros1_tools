3a1d973629679ba00557a150ceb93ffb9bd56279
Patricio Loncomilla <ploncomi@gmail.com>
2024-06-14 09:59:30 -0400

    Update ros2_to_ros1_bash_script.sh

--- ros2_to_ros1_bash_script.sh
+++ ros2_to_ros1_bash_script.sh
@@ -1,3 +1,7 @@
+alias rosnode='ros2 node'
+alias rosservice='ros2 service'
+alias rosbag='ros2 bag'
+
 _roscd() 
 {
     local cur prev opts
@@ -13,3 +17,12 @@ _roscd()
     fi
 }
 complete -F _roscd roscd
+
+
+function rostopic() {
+    if [[ "info" = "$1" ]] ; then
+      ros2 topic info "--verbose" "${@:2}"
+    else
+      ros2 topic "${@:1}"
+    fi
+}
